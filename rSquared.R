14815d1de6c39e3dbe9052cf3d0e6af83bbc6dfe
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2016-04-10 18:51:09 -0400

    Edited comments

--- rSquared.R
+++ rSquared.R
@@ -1,6 +1,6 @@
 # Calculates the R squared value of two vectors: actualValues and predictedValues.
 rSquared <- function(actualValues, predictedValues) {
-  # R^2 = (sum_(i=1 to n) of (actualValues[i] - predictedValues[i])^2) / (sum_(i=1 to n) of (actualValues[i] - mean of actualValues)^2)
+  # R^2 = 1 - (sum_(i=1 to n) of (actualValues[i] - predictedValues[i])^2) / (sum_(i=1 to n) of (actualValues[i] - mean of actualValues)^2)
   value <- 1 - (sum((actualValues - predictedValues)^2) / sum((actualValues - mean(actualValues))^2))
   
   return(value)
