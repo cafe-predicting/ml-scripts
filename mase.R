ea4f262e00c7fc95f5c1a4c577eac7ea9b6a29dd
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2016-04-13 19:32:08 -0400

    Added more detailed comments.

--- mase.R
+++ mase.R
@@ -3,16 +3,29 @@
 # Mean Absolute Scaled Error
 # See https://en.wikipedia.org/wiki/Mean_absolute_scaled_error
 mase <- function(actualValues, predictedValues) {
+  # Throw exception if actualValues or predictedValues are of length 1.
   if (length(actualValues) < 2 || length(predictedValues) < 2) {
     stop("Incorrect number of rows")
   }
   
+  # Mean Absolute Scaled Error is calculated by dividing the forecast error by the average forecast error.
+  # The forecast error is the sum of the absoulte value of y_i - f_i
+  #   where y is the vector 1 to n of actual values
+  #   and f is the vector 1 to n of predicted values
+  # The average forecast error is calculated by n / (n - 1)
+  #   multiplied by the sum from i = 2 to n of the absolute value of y_i - y_(i-1).
+  
+  # The denominator value calculates the average forecast error.
   denominator <- 0
+  
+  # The for loop calculates the summation portion first.
   for (i in 2:length(actualValues)) {
     denominator <- denominator + (abs(actualValues[i] - actualValues[(i-1)]))
   }
   
+  # Here the n/(n-1) portion is calculated and multiplied to the summation portion.
   denominator <- (length(actualValues) / (length(actualValues)-1)) * denominator
-    
+  
+  # Calculate the forecast error and divide it by the average forecast error defined above.
   return((sum(abs(actualValues - predictedValues))) / denominator)
 }
--- rSquared.R
+++ rSquared.R
@@ -3,7 +3,13 @@
 # Calculates the R squared value of two vectors: actualValues and predictedValues.
 # See https://en.wikipedia.org/wiki/Coefficient_of_determination#Definitions
 rSquared <- function(actualValues, predictedValues) {
-  # R^2 = 1 - (sum_(i=1 to n) of (actualValues[i] - predictedValues[i])^2) / (sum_(i=1 to n) of (actualValues[i] - mean of actualValues)^2)
+  # R^2 is calculated by 1 minus the residual sum of squares divided by the total sum of squares.
+  # The residual sum of squares is the sum of (y_i - f_i) squared
+  #   where y is the vector 1 to n of actual values
+  #   and f is the vector 1 to n of predicted values
+  # The total sum of squares is the sum of (y_i - y_mean) squared
+  #   where y_mean is the mean of the y vector.
+  # In the code below, actualValues is the vector y and predictedValues is the vector f.
   value <- 1 - (sum((actualValues - predictedValues)^2) / sum((actualValues - mean(actualValues))^2))
   
   return(value)
